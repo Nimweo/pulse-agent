2a74a0c9452f32076a5efe48b8b55d8f9717fb5f
Maciej Mierzejewski <maciej.mierzejewski.01@gmail.com>
2026-08-29 13:10:07 +0200

    (fix) align agent version with release tag

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -20,7 +20,7 @@ import (
 )
 
 const (
-	version                 = "0.12.0"
+	version                 = "0.12.1"
 	authenticationExitCode  = 78
 	linuxSystemConfigPath   = "/etc/nimweo/pulse-agent/config.yaml"
 	linuxUpdateStatePath    = "/var/lib/pulse-agent-updater/update-state.json"
