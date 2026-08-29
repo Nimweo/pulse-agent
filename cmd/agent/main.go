001df1851f1c674c3b3ccf3b3ce1a60c8d034b02
Maciej Mierzejewski <maciej.mierzejewski.01@gmail.com>
2026-08-29 12:11:24 +0200

    (chore) bump version to 0.10.2

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -20,7 +20,7 @@ import (
 )
 
 const (
-	version                 = "0.10.1"
+	version                 = "0.10.2"
 	authenticationExitCode  = 78
 	linuxSystemConfigPath   = "/etc/nimweo/pulse-agent/config.yaml"
 	linuxUpdateStatePath    = "/var/lib/pulse-agent-updater/update-state.json"
